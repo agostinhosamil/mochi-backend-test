24f2994ac5b48b3f8cccb30e4c91cc22acd51558
agostinhosamil <agostinhosaml832@gmail.com>
2021-01-29 05:56:46 -0800

    fix: Configure cors

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-import express from 'express'
-import cors from 'cors'
 import 'dotenv/config'
+import cors from 'cors'
+import express from 'express'
 
 import { Routes } from './config/routes'
 
 const app = express()
 
+app.use(cors())
 app.use(Routes)
 
-app.listen(3333, () => 
+app.listen(process.env.PORT || 3333, () => 
 	console.log('server running..!')
 )
